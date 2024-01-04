53bf2648b7daaaad3b18f801eb822da995aee938
MatejaMusa <matejamusa6@gmail.com>
2024-01-04 19:26:20 +0100

    user profile settings enable/disable implemented

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, tap, throwError } from 'rxjs';
 import { CustomHttpResponse, Profile } from '../interface/appstates';
@@ -27,15 +27,16 @@ export class UserService {
     catchError(this.handleError)
   );
 
-  profile$ = (email: string, code: string) => <Observable<CustomHttpResponse<Profile>>>
+  profile$ = () => <Observable<CustomHttpResponse<Profile>>>
   this.http.get<CustomHttpResponse<Profile>>
-  (`${this.server}/user/profile`)
+  (`${this.server}/user/profile`, { headers: new HttpHeaders().set('Authorization', 'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJJTlZPSUNFX0ZMT1dfRE9PIiwiYXVkIjoiQ1VTVE9NRVJfTUFOQUdFTUVOVF9TRVJWSUNFIiwiaWF0IjoxNzA0MzkyNjU5LCJzdWIiOiJtYXRlamFAZ21haWwuY29tIiwiYXV0aG9yaXRpZXMiOlsiUkVBRDpVU0VSIiwiUkVBRDpDVVNUT01FUiIsIlVQREFURTpVU0VSIiwiVVBEQVRFOkNVU1RPTUVSIiwiQ1JFQVRFOlVTRVIiLCJDUkVBVEU6Q1VTVE9NRVIiXSwiZXhwIjoxNzA0MzkzNjU5fQ.3raesE4Jg7MKfFxRbvGSICB3P_eDTCyGoddpU7h8zLlU9lahirbYCgBg-U7LjpVkHQ-qiz1z8fVHcCjC8EVHQQ')})
   .pipe(
     tap(console.log),
     catchError(this.handleError)
   );
 
   private handleError(error: HttpErrorResponse): Observable<never> {
+    console.log(error);
     let errorMessage: string;
     if(error.error instanceof ErrorEvent) {
       errorMessage =  `A client error occurred - ${error.error.message}`;
